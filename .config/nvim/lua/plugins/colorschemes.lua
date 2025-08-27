28f208bdfca3c3c2f8c554e0e52e1d990f6fe0a1
Viktor Paraj <viktor.paraj@gmail.com>
2025-08-27 21:24:45 +0200

    Added oxocarbon.nvim

--- .config/nvim/lua/plugins/colorschemes.lua
+++ .config/nvim/lua/plugins/colorschemes.lua
@@ -2,7 +2,8 @@ return {
   { "vague2k/vague.nvim", name = "vague", lazy = false },
   { "slugbyte/lackluster.nvim", name = "lackluster", lazy = false },
   { "morhetz/gruvbox", name = "gruvbox", lazy = false },
-  { "rose-pine/neovim", name = "rose-pine", lazy = false, disable_background = true },
+  { "rose-pine/neovim", name = "rose-pine", lazy = false},
+  { "nyoom-engineering/oxocarbon.nvim", name = "oxocarbon", lazy = false },
 
   {
     "zaldih/themery.nvim",
@@ -11,7 +12,7 @@ return {
       vim.g.gruvbox_contrast_dark = "hard"
 
       require("themery").setup({
-        themes = {"gruvbox", "lackluster", "vague", "rose-pine"},
+        themes = {"gruvbox", "lackluster", "vague", "rose-pine", "oxocarbon"},
         livePreview = true,
         save = true,
       })
