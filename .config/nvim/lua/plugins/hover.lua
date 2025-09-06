c210fb0572e45b081739fa2daceb205cecfdbdc9
Viktor Paraj <viktor.paraj@gmail.com>
2025-09-07 01:17:51 +0200

    Update bang

--- .config/nvim/lua/plugins/hover.lua
+++ .config/nvim/lua/plugins/hover.lua
@@ -27,9 +27,6 @@ return {
         }
 
         vim.keymap.set("n", "K", require("hover").hover, {desc = "hover.nvim"})
-
-        -- Mouse support
-        vim.keymap.set('n', '<MouseMove>', require('hover').hover_mouse, { desc = "hover.nvim (mouse)" })
         vim.o.mousemoveevent = true
     end
 }
